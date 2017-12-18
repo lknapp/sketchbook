c10d1d926c1a7475834f9139e7472ecf48b84e83
lk <louisknapp@gmail.com>
2017-12-18 15:56:34 +0000

    lk | improve bezier script

--- beziers/beziers.js
+++ beziers/beziers.js
@@ -1,15 +1,34 @@
+var colors;
 
 function setup() {
   createCanvas(screen.width, screen.height);
   frame = 0;
   colorMode(HSB, 100);
+  colors = [];
+  for (var i = 0; i < 30; i ++) {
+    colors << 101;
+  }
 }
 
 function draw() {
   background(0);
-  stroke(100);
+  strokeWeight(15);
   noFill();
-  for (var i = 0; i < 200; i += 20) {
-    bezier(mouseX-(i/2.0), 40+i, 410, 20, 440, 300, 240-(i/16.0), 300+(i/8.0));
+  centerY = screen.height / 2.0;
+  for (var i = 0; i < 30; i ++) {
+    offset = i*40 - 600;
+
+    randomN = Math.random();
+    if (randomN < 0.03) {
+      colors[i] = Math.random()*100;
+    } else if (randomN < 0.2) {
+      colors[i] = 101;
+    }
+    if(colors[i] == 101) {
+      stroke(100);
+    } else {
+      stroke(colors[i], 100, 100);
+    }
+    bezier(0, centerY + offset - mouseY/2, 410 - offset + mouseX, 20 + mouseY, 440 - mouseX, 300 + mouseY, screen.width, centerY - offset + mouseY/2);
   }
 }
